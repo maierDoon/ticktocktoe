43aad5d5f9295f274529037cb70f7fd4135f6891
maierDoon <maierlasdun@gmail.com>
2016-06-29 15:48:19 -0400

    working on smart computer

--- src/main/scala/ticktocktoe/Main.scala
+++ src/main/scala/ticktocktoe/Main.scala
@@ -125,29 +125,89 @@ object Main extends App {
   }
 
   def computersTurn(playerOne: Player, playerTwo: Player): Coord = {
-
-   //needToDefend(playerOne, playerTwo, 0).getOrElse((random.nextInt(3),random.nextInt(3)))
-
-
-      val col = random.nextInt(3)
+   findSpot(playerOne, playerTwo)
+      /*val col = random.nextInt(3)
       val row = random.nextInt(3)
-
-      (row, col)
+      (row, col)*/
   }
 
 
-  //def needToDefend(playerOne: Player, playerTwo: Player, line: Int): Option[Coord] = {
-    /*def row (t: Tuple2) : Boolean =
-      t._1 == line
+  def findSpot(playerOne: Player, playerTwo: Player): Coord = {
 
-     def col (t: Tuple2) : Boolean =
-       t._2 == line*/
-      /*if (playerOne.getSpots.count(_._1 == line) == 2 &&
+   /* def needToDefend(func: (Coord, Int) => Boolean, line: Int, isRow: Boolean): Option[Coord] = {
+        if (playerOne.getSpots.count(x => func(x, line)) == 2 &&
+        !playerTwo.getSpots.exists(x => func(x, line))) {
+          if(isRow){
+            Some(playerOne.getSpots.filter(x => func(x, line)).fold((line, 3))((x, y) => (line, x._2 - y._2)))  //reduce{(x, y) => (x._1, x._2 + y._2)}
+          }else {
+            Some(playerOne.getSpots.filter(x => func(x, line)).fold((3, line))((x, y) => (x._1 - y._1, line)))
+          }
+      } else
+        None
+    }
+
+    def row (c: Coord, line: Int) : Boolean =
+      c._1 == line
+    def col (c: Coord, line: Int) : Boolean =
+      c._2 == line
+
+    needToDefend(row, 0, true).
+      getOrElse(needToDefend(row, 1, true).
+        getOrElse(needToDefend(row, 2, true).
+          getOrElse(needToDefend(col, 0, false).
+            getOrElse(needToDefend(col, 1, false).
+              getOrElse(needToDefend(col, 2, false).
+                getOrElse(random.nextInt(3),random.nextInt(3)))))))*/
+
+
+    /*def checkRows(line: Int): Option[Coord] = {
+      if (playerOne.getSpots.count(_._1 == line) == 2 &&
         !playerTwo.getSpots.exists(_._1 == line)) {
         Some(playerOne.getSpots.filter(_._1 == line).fold((line, 3))((x, y) => (line, x._2 - y._2)))  //reduce{(x, y) => (x._1, x._2 + y._2)}
       } else
         None
-  }*/
+    }
+    checkRows(0).getOrElse(
+      checkRows(1).getOrElse(
+        checkRows(2).getOrElse(
+          (random.nextInt(3),random.nextInt(3)))))*/
+
+    def needToDefend(line: Int, isRow: Boolean): Option[Coord] = {
+
+      /////
+      (0 to 2).count(a => playerOne.getSpots.contains(((a, a)) == 2))  //notexist
+
+      val a = playerOne.getSpots.filter(a => a._1 == a._2)
+      a.fold((3, 3))((x, y) => (x._1 - y._1, x._2 - y._2))
+
+      //List((0,0),(1,1), (2,2)).filterNot((a, b) => (a, b))
+      //List.tabulate(3, 2)((a, b) => a + b)
+
+
+      /////////
+
+      val func = (c: Coord, line: Int) =>  if(isRow) c._1 == line else c._2 == line
+
+      if (playerOne.getSpots.count(x => func(x, line)) == 2 &&
+        !playerTwo.getSpots.exists(x => func(x, line))) {
+        if(isRow){
+          Some(playerOne.getSpots.filter(x => func(x, line)).fold((line, 3))((x, y) => (line, x._2 - y._2)))  //reduce{(x, y) => (x._1, x._2 + y._2)}
+        }else {
+          Some(playerOne.getSpots.filter(x => func(x, line)).fold((3, line))((x, y) => (x._1 - y._1, line)))
+        }
+      } else
+        None
+    }
+
+    needToDefend(0, isRow = true).
+      getOrElse(needToDefend(1, isRow = true).
+        getOrElse(needToDefend(2, isRow = true).
+          getOrElse(needToDefend(0, isRow = false).
+            getOrElse(needToDefend(1, isRow = false).
+              getOrElse(needToDefend(2, isRow = false).
+                getOrElse(random.nextInt(3),random.nextInt(3)))))))
+
+  }
 
   def createAllPlayers(): (Player, Player) = {
     def createPlayer(): Player = {
