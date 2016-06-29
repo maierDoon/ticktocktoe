e5c846a61657ef323cc6aae62c0e394f72aa52d5
maierDoon <maierlasdun@gmail.com>
2016-06-29 18:30:33 -0400

    working on smart computer

--- src/main/scala/ticktocktoe/Main.scala
+++ src/main/scala/ticktocktoe/Main.scala
@@ -59,8 +59,8 @@ object Main extends App {
     }
   }
 
-  def stopGame(player: Player): Boolean = {
-    val spots = player.getSpots
+  def stopGame(me: Player, oponent: Player): Boolean = {
+    val spots = me.getSpots
 
     def isRowFilled(row: Int): Boolean =
       (0 to 2).forall(col => spots.contains((row, col)))
@@ -79,10 +79,10 @@ object Main extends App {
         (0 to 2).forall(a => spots.contains((a, 2 - a)))
 
     def isTie: Boolean =
-      playerTwo.getSpots.length + playerOne.getSpots.length == 9
+      oponent.getSpots.length + spots.length == 9
 
     if (isAnyRowFilled || isAnyColumnFilled || isDiagonalFilled) {
-      println(player.name + " is a winner !!")
+      println(me.name + " is a winner !!")
       true
     } else if (isTie) {
       println("no winner !!")
@@ -134,58 +134,7 @@ object Main extends App {
 
   def findSpot(playerOne: Player, playerTwo: Player): Coord = {
 
-   /* def needToDefend(func: (Coord, Int) => Boolean, line: Int, isRow: Boolean): Option[Coord] = {
-        if (playerOne.getSpots.count(x => func(x, line)) == 2 &&
-        !playerTwo.getSpots.exists(x => func(x, line))) {
-          if(isRow){
-            Some(playerOne.getSpots.filter(x => func(x, line)).fold((line, 3))((x, y) => (line, x._2 - y._2)))  //reduce{(x, y) => (x._1, x._2 + y._2)}
-          }else {
-            Some(playerOne.getSpots.filter(x => func(x, line)).fold((3, line))((x, y) => (x._1 - y._1, line)))
-          }
-      } else
-        None
-    }
-
-    def row (c: Coord, line: Int) : Boolean =
-      c._1 == line
-    def col (c: Coord, line: Int) : Boolean =
-      c._2 == line
-
-    needToDefend(row, 0, true).
-      getOrElse(needToDefend(row, 1, true).
-        getOrElse(needToDefend(row, 2, true).
-          getOrElse(needToDefend(col, 0, false).
-            getOrElse(needToDefend(col, 1, false).
-              getOrElse(needToDefend(col, 2, false).
-                getOrElse(random.nextInt(3),random.nextInt(3)))))))*/
-
-
-    /*def checkRows(line: Int): Option[Coord] = {
-      if (playerOne.getSpots.count(_._1 == line) == 2 &&
-        !playerTwo.getSpots.exists(_._1 == line)) {
-        Some(playerOne.getSpots.filter(_._1 == line).fold((line, 3))((x, y) => (line, x._2 - y._2)))  //reduce{(x, y) => (x._1, x._2 + y._2)}
-      } else
-        None
-    }
-    checkRows(0).getOrElse(
-      checkRows(1).getOrElse(
-        checkRows(2).getOrElse(
-          (random.nextInt(3),random.nextInt(3)))))*/
-
     def needToDefend(line: Int, isRow: Boolean): Option[Coord] = {
-
-      /////
-      (0 to 2).count(a => playerOne.getSpots.contains(((a, a)) == 2))  //notexist
-
-      val a = playerOne.getSpots.filter(a => a._1 == a._2)
-      a.fold((3, 3))((x, y) => (x._1 - y._1, x._2 - y._2))
-
-      //List((0,0),(1,1), (2,2)).filterNot((a, b) => (a, b))
-      //List.tabulate(3, 2)((a, b) => a + b)
-
-
-      /////////
-
       val func = (c: Coord, line: Int) =>  if(isRow) c._1 == line else c._2 == line
 
       if (playerOne.getSpots.count(x => func(x, line)) == 2 &&
@@ -199,13 +148,34 @@ object Main extends App {
         None
     }
 
+  def defendRightDiagonal: Option[Coord] = {
+    if(playerOne.getSpots.count(spot => spot._1 == 2 - spot._2) == 2 &&
+      playerTwo.getSpots.count(spot => spot._1 == 2 - spot._2) == 0) {
+      Some(playerOne.getSpots.filter(spot => spot._1 == 2 - spot._2).fold((3, 3))((old, current) => (old._1 - current._1, old._2 - current._2)))
+    } else {
+      None
+    }
+  }
+
+  def defendLeftDiagonal: Option[Coord] = {
+    if(playerOne.getSpots.count(spot => spot._1 == spot._2) == 2 &&
+      playerTwo.getSpots.count(spot => spot._1 == spot._2) == 0){
+      Some(playerOne.getSpots.filter(spot => spot._1 == spot._2).fold((3, 3))((old, current) => (old._1 - current._1, old._2 - current._2)))
+    }else{
+      None
+    }
+  }
+
+
     needToDefend(0, isRow = true).
       getOrElse(needToDefend(1, isRow = true).
         getOrElse(needToDefend(2, isRow = true).
           getOrElse(needToDefend(0, isRow = false).
             getOrElse(needToDefend(1, isRow = false).
               getOrElse(needToDefend(2, isRow = false).
-                getOrElse(random.nextInt(3),random.nextInt(3)))))))
+                getOrElse(defendRightDiagonal.
+                  getOrElse(defendLeftDiagonal.
+                   getOrElse(random.nextInt(3),random.nextInt(3)))))))))
 
   }
 
@@ -271,12 +241,12 @@ object Main extends App {
     val personOne = playerOneAttempt(playerOne, playerTwo)
     printBoardToConsole(personOne, playerTwo)
 
-    if (!stopGame(personOne)) {
+    if (!stopGame(personOne, playerTwo)) {
       println(playerTwo.name + ", please go")
       val personTwo = playerTwoAttempt(personOne, playerTwo)
       printBoardToConsole(personOne, personTwo)
 
-      if (!stopGame(personTwo))
+      if (!stopGame(personTwo, personOne))
         playing(personOne, personTwo)
     }
   }
